8056bb97c4aee8524a438431535f4821bb9ddc13
leri-berry <valeria.ropot2@gmail.com>
2021-03-29 22:54:15 +0300

    add tasks for the lesson 1

---
+++ define_triangle.rb
@@ -0,0 +1,11 @@
+puts "Enter 3 side of the triangle"
+sides = gets.chomp
+integer_array = sides.split.map(&:to_i).sort
+if integer_array[2]**2 == integer_array[0]**2 + integer_array[1]**2
+    puts "Tringle is right"
+elsif integer_array[0] == integer_array[1] && integer_array[1] == integer_array[2] && integer_array[2] == integer_array[0]
+    puts "Triangle is equilateral"
+elsif integer_array[0] == integer_array[1] || integer_array[1] == integer_array[2] || integer_array[2] == integer_array[0]
+    puts "Triangle is isosceles"
+else puts "Triangle is not defined"
+end
