2b054d71f280897eeefc8ff0262f3e65658eb627
leri-berry <valeria.ropot2@gmail.com>
2021-04-03 16:28:58 +0300

    edit files for lesson1

--- lesson_1/ideal_weight.rb
+++ lesson_1/ideal_weight.rb
@@ -1,10 +1,10 @@
 puts 'What is your name?'
 name = gets.chomp
 puts 'What is your height?'
-height = gets.chomp
-ideal_weight = (height.to_i - 110)*1.15
+height = gets.to_i
+ideal_weight = (height - 110)*1.15
 if ideal_weight < 0 
-puts "#{name}, your weight is already optimal"
-else 
-puts "#{name}, your ideal weight is #{ideal_weight.round(1)}"
+  puts "#{name}, your weight is already optimal"
+else
+  puts "#{name}, your ideal weight is #{ideal_weight.round(1)}"
 end
--- lesson_1/quadratic equation.rb
+++ lesson_1/quadratic equation.rb
@@ -1,21 +1,21 @@
-puts "Введите 3 коэфициента a, b, c"
+puts 'Введите 3 коэфициента a, b, c'
 coefficient = gets.chomp
 integer = coefficient.split.map(&:to_i)
-a = integer[0]; 2
-b = integer[1]; 3
-c = integer[2]; 4
-d = b**2 - 4*a*c
-if d > 0 
-    c = Math.sqrt(d)
-    x1 = (-b + c)/(2*a)
-    x2 = (-b - c)/(2*a)
-puts "Дискриминант: #{d}"
-puts "Корень1: #{x1.round(2)}"
-puts "Корень2: #{x2.round(2)}"
-elsif d == 0
-    x1 = –b/(2*a)
+a = integer[0];
+b = integer[1];
+c = integer[2];
+d = b**2 - 4 * a * c
+if d > 0
+  c = Math.sqrt(d)
+  x1 = (-b + c) / (2 * a)
+  x2 = (-b - c) / (2 * a)
+  puts "Дискриминант: #{d}"
+  puts "Корень1: #{x1.round(2)}"
+  puts "Корень2: #{x2.round(2)}"
+  elsif d == 0
+    x1 = –b / (2 * a)
     puts "Дискриминант: #{d}"
     puts "Корень: #{x1}"
-else d < 0
+  else d < 0
     puts "Корней нет"
-end
+  end
