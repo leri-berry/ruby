b6858e3bb551c920d88e520756766a70c10deead
leri-berry <valeria.ropot2@gmail.com>
2021-04-20 23:29:24 +0300

    correct spaces

--- lesson_2/fibonacchi_array.rb
+++ lesson_2/fibonacchi_array.rb
@@ -4,9 +4,8 @@ fibonacci_numbers = [0, 1]
 element = 1
 
 while element < 100
-	fibonacci_numbers << element
-	element = fibonacci_numbers[-2] + fibonacci_numbers[-1]
+  fibonacci_numbers << element
+  element = fibonacci_numbers[-2] + fibonacci_numbers[-1]
 end
 
 puts fibonacci_numbers
-
